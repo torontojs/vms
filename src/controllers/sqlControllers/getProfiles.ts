4d1b62b7d4662a50d965a7fac2d9f665fdf39cbd
sounmind <devleesche@gmail.com>
2024-12-24 10:51:47 -0500

    fix: getProfileById, getAllProfiles
    - Changed the table name(profiles -> profile)
    
    Co-authored-by: Manvinderjit Rupal <manvinderjit@users.noreply.github.com>

--- src/controllers/sqlControllers/getProfiles.ts
+++ src/controllers/sqlControllers/getProfiles.ts
@@ -1,29 +1,26 @@
 import type { Context } from 'hono';
 import { StatusCodes } from '../../constants/status-codes.ts';
-import type { Profile } from '../../db/profile';
 
 export const getProfileById = async (context: Context<EnvironmentBindings>) => {
-	const userId = context.req.param("id");
+	const userId = context.req.param('id');
 	try {
-	  const { results } = await context.env.database.prepare(
-		"SELECT * FROM profiles WHERE id = ?",
-	  )
-		.bind(userId)
-		.run<Profile>();
-	  return context.json(results);
+		const { results } = await context.env.database
+			.prepare('SELECT * FROM profile WHERE id = ?')
+			.bind(userId)
+			.run();
+		return context.json(results);
 	} catch (e) {
-	  return context.json({ err: e.message }, StatusCodes.NOT_FOUND);
+		return context.json({ err: e.message }, StatusCodes.NOT_FOUND);
 	}
-  };
+};
 
-  export const getAllProfiles = async (context: Context) => {
+export const getAllProfiles = async (context: Context) => {
 	try {
-	  let { results } = await context.env.database.prepare(
-		"SELECT * FROM profiles",
-	  )
-		.all();
-	  return context.json(results);
+		const { results } = await context.env.database
+			.prepare('SELECT * FROM profile')
+			.all();
+		return context.json(results);
 	} catch (e) {
-	  return context.json({ err: e.message }, StatusCodes.NOT_FOUND);
+		return context.json({ err: e.message }, StatusCodes.NOT_FOUND);
 	}
-  };
+};
