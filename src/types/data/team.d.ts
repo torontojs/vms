7d12537bbccaa478e82e35c646279bca308f59a5
Ken Beaudin <30326135+kbventures@users.noreply.github.com>
2025-01-15 15:42:04 -0500

    Update team.d.ts

--- src/types/data/team.d.ts
+++ src/types/data/team.d.ts
@@ -1,20 +1,22 @@
 export interface Team {
-	id: string;
-	schemaVersion: number;
-	name: string;
-	description?: string;
-	happenedAt: string;
-	insertedAt: string;
+  id: string;
+  schemaVersion: number;
+  name: string;
+  description?: string;
+  happenedAt: string;
+  insertedAt: string;
 }
 
 export interface NewTeamData {
-	name: string;
-	happenedAt: string;
-	description?: string;
+  name: string;
+  happenedAt: string;
+  description?: string;
 }
 
 export interface UpdateTeamData {
-	name?: string;
-	happenedAt?: string;
-	description?: string;
+  name?: string;
+  happenedAt?: string;
+  description?: string;
 }
+
+export type BaseTeamData = Pick<Team, 'name' | 'happenedAt' | 'description'>;
